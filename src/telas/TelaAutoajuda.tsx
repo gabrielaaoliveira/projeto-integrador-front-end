e8aacfa9c39cb6a224033bcfd3a56f135ce455e3
Gabriela Oliveira <gabriella04022003@gmail.com>
2022-06-30 21:58:07 -0300

    Tela auto ajuda tendo sua conclusão quase que final com o html correto e css já ajustado

--- src/telas/TelaAutoajuda.tsx
+++ src/telas/TelaAutoajuda.tsx
@@ -11,18 +11,18 @@ const TelaAutoajuda = function () {
     return (
         <Tela>
             <div className='TelaMapa'>
-                <h1>Auto Ajuda</h1>
+                <h1>Crise de Ansiedade</h1>
                 <div className='Mapas'>
                     <div className='Mapa'>
                         <div className='Imagem'>
                             <img src={ansiedade} alt= "" />
                         </div>
                         <div className='Titulo'>
-                        < h4>"Como sair da crise de ansiedade?"</h4> 
+                        < h4>Como sair da crise de ansiedade?</h4> 
                         </div>
                         <div className='Texto'>
                             <p>
-                                Passar por um momento de crise não é nada fácil e ter um ente querido que tem estes episódios é tão difícil quanto. É importante conhecer formas de aliviar os sintomas e conseguir se acalmar. Apartir daqui segue dicas de como lidar com este momento tão delicado.
+                                Passar por um momento de crise não é nada fácil e ter um ente querido que tem estes episódios é tão difícil quanto. É importante conhecer formas de aliviar os sintomas e conseguir se acalmar. A partir daqui segue dicas de como lidar com este momento tão delicado.
                             </p>
                         </div>
                     </div>
@@ -31,15 +31,12 @@ const TelaAutoajuda = function () {
                         <div className='Imagem'>
                             <img src={ansiedade2} alt='' />
                         </div>
-                        <div className='Indice'>
-                            <div className='Barra'>
-                                <div className='Porcentagem' style={{width: '10%'}}></div>
-                            </div>
-                            <div className='Legenda'>10% de pessoas no Brasil</div>
+                        <div className='Titulo'>
+                        < h4>Desvie atenção dos sintomas!</h4> 
                         </div>
                         <div className='Texto'>
                             <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ipsam maiores porro harum officiis eius repellat, id hic et quod fuga rerum iste? Non consequuntur sapiente vel corrupti. Nulla, optio.
+                               Em um momento de crise de ansiedade é muito comum o foco nos sintomas que com o passar do tempo parece estar tendo um infarto. É importante que neste momento você foque em uma atividade como a respiracação consciente.  
                             </p>
                         </div>
                     </div>
@@ -48,15 +45,12 @@ const TelaAutoajuda = function () {
                         <div className='Imagem'>
                             <img src={ansiedade3} alt='' />
                         </div>
-                        <div className='Indice'>
-                            <div className='Barra'>
-                                <div className='Porcentagem' style={{width: '10%'}}></div>
-                            </div>
-                            <div className='Legenda'>10% de pessoas no Brasil</div>
+                        <div className='Titulo'>
+                        < h4>Respiração consciente!</h4> 
                         </div>
                         <div className='Texto'>
                             <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ipsam maiores porro harum officiis eius repellat, id hic et quod fuga rerum iste? Non consequuntur sapiente vel corrupti. Nulla, optio.
+                             A respiração consciente é importante para conseguir diminuir a tenção. Se baseia inspirar por 4 segundos, segurar a respiração por 1 segundo e expirar por 4 segundos.
                             </p>
                         </div>
                     </div>
@@ -65,15 +59,12 @@ const TelaAutoajuda = function () {
                         <div className='Imagem'>
                             <img src={ansiedade4} alt='' />
                         </div>
-                        <div className='Indice'>
-                            <div className='Barra'>
-                                <div className='Porcentagem' style={{width: '10%'}}></div>
-                            </div>
-                            <div className='Legenda'>10% de pessoas no Brasil</div>
+                        <div className='Titulo'>
+                        < h4>Relaxe os músculos!</h4> 
                         </div>
                         <div className='Texto'>
                             <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ipsam maiores porro harum officiis eius repellat, id hic et quod fuga rerum iste? Non consequuntur sapiente vel corrupti. Nulla, optio.
+                                Os músculos tendem a se contrair cada vez mais em uma forma de defesa no momento de crise de ansiedade. No entando, este "modo defesa" acava trazendo um grande desconforto com a contração dos músculos. Por isso é crucial que tente ao máximo relaxar.
                             </p>
                         </div>
                     </div>
@@ -82,15 +73,12 @@ const TelaAutoajuda = function () {
                         <div className='Imagem'>
                             <img src={ansiedade5} alt='' />
                         </div>
-                        <div className='Indice'>
-                            <div className='Barra'>
-                                <div className='Porcentagem' style={{width: '10%'}}></div>
-                            </div>
-                            <div className='Legenda'>10% de pessoas no Brasil</div>
+                        <div className='Titulo'>
+                        < h4>Se distraia!</h4> 
                         </div>
                         <div className='Texto'>
                             <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ipsam maiores porro harum officiis eius repellat, id hic et quod fuga rerum iste? Non consequuntur sapiente vel corrupti. Nulla, optio.
+                                Encontre algo que goste, como: um filme, uma música ou outra atividade que te deixe calmo. Ao fazer algo que se sente confortável, logo perceberá que sua respiração estará mais tranquila e seu corpo vai relaxando gradativamente.
                             </p>
                         </div>
                     </div>
@@ -99,15 +87,100 @@ const TelaAutoajuda = function () {
                         <div className='Imagem'>
                             <img src={ansiedade6} alt='' />
                         </div>
-                        <div className='Indice'>
-                            <div className='Barra'>
-                                <div className='Porcentagem' style={{width: '10%'}}></div>
-                            </div>
-                            <div className='Legenda'>10% de pessoas no Brasil</div>
+                        <div className='Titulo'>
+                        < h4>Melhorou?</h4> 
                         </div>
                         <div className='Texto'>
                             <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ipsam maiores porro harum officiis eius repellat, id hic et quod fuga rerum iste? Non consequuntur sapiente vel corrupti. Nulla, optio.
+                                Não é fácil passar por uma crise de ansiedade e a constância dela nos leva a refletir sobre buscar uma ajuda profissional para que não afete seu cotidiano e tenha uma vida saudável. Lembre-se que sentir é normal e não esta sozinho nessa! Estamos aqui para te ajudar.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='TelaMapa'>
+                <h1>Crise de Pânico</h1>
+                <div className='Mapas'>
+                    <div className='Mapa'>
+                        <div className='Imagem'>
+                            <img src={ansiedade} alt= "" />
+                        </div>
+                        <div className='Titulo'>
+                        < h4>Como sair da crise de pânico?</h4> 
+                        </div>
+                        <div className='Texto'>
+                            <p>
+                                A crise de pânico causa uma sensação enorme de medo, mas é importante estar ciente que é algo momentâneo, Com isso em mente, é crucial buscar opções que te façam se sentir confortável. Com isso, sente-se, respire e saiba que os pensamentos que o incomoda passará em breve.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className='Mapa'>
+                        <div className='Imagem'>
+                            <img src={ansiedade2} alt='' />
+                        </div>
+                        <div className='Titulo'>
+                        < h4>Feche os olhos e respire</h4> 
+                        </div>
+                        <div className='Texto'>
+                            <p>
+                               Com a crise de pânico vem a sensação de sair da realidade e não pertencimento. Um exercício de ancoramento da realidade é recorrer a texturas de objetos que são familiares ao seu redor, isso trará um conforto de se manter pensante ao permanecimento da realidade.  
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className='Mapa'>
+                        <div className='Imagem'>
+                            <img src={ansiedade3} alt='' />
+                        </div>
+                        <div className='Titulo'>
+                        < h4>Se concentre em algum objeto</h4> 
+                        </div>
+                        <div className='Texto'>
+                            <p>
+                             Como anteriormente o tato foi um método essencial para trazer a realidade, a concentração em um objeto ajudará tanto quanto. Neste momento da crise você se sente desamparado e com um enorme medo, mas que saber estar no presente, te mantém ali focado no ambiente ao redor e desfocando dos pensamentos.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className='Mapa'>
+                        <div className='Imagem'>
+                            <img src={ansiedade4} alt='' />
+                        </div>
+                        <div className='Titulo'>
+                        < h4>Lembre-se de uma memória feliz</h4> 
+                        </div>
+                        <div className='Texto'>
+                            <p>
+                                Lembrar de uma memória feliz fará com que seu cérebro libere substâncias que combaterão as sensações ruins. Assim que conseguir substituir pensamentos ruins por pensamentos bons, logo conseguirá se sentir mais tranquilo e o turbilhão de pensamentos irá se decipar.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className='Mapa'>
+                        <div className='Imagem'>
+                            <img src={ansiedade5} alt='' />
+                        </div>
+                        <div className='Titulo'>
+                        < h4>Sinta um aroma que te tranquilize</h4> 
+                        </div>
+                        <div className='Texto'>
+                            <p>
+                            Percebeu que as dicas foram voltadas para os sentidos? E um deles que ajuda neste momento é o olfato, sentir algo que te remeta a tranquilidade te faz se sentir mais confortável e os pensamentos vão diminuindo a intensidade no decorrer dos minutos seguintes.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className='Mapa'>
+                        <div className='Imagem'>
+                            <img src={ansiedade6} alt='' />
+                        </div>
+                        <div className='Titulo'>
+                        < h4>Melhorou?</h4> 
+                        </div>
+                        <div className='Texto'>
+                            <p>
+                                Não é fácil passar por uma crise de ansiedade e a constância dela nos leva a refletir sobre buscar uma ajuda profissional para que não afete seu cotidiano e tenha uma vida saudável. Lembre-se que sentir é normal e não esta sozinho nessa! Estamos aqui para te ajudar.
                             </p>
                         </div>
                     </div>

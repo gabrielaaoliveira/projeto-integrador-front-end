7771a07b5b4ab58d409b5b8acf0b8abe54f153a2
Ana Alice <as4714391@gmail.com>
2022-07-05 20:47:04 -0300

    menuatualizado

--- src/componentes/Menu.tsx
+++ src/componentes/Menu.tsx
@@ -1,11 +1,15 @@
 import './Menu.css';
 import './CampoBusca';
+import { Link } from 'react-router-dom';
 
 const Menu = function() {
     return (
         <nav className="Menu">
-            <a className="MenuItem" href="">Mapa mental</a>
-            <a className="MenuItem" href="/autoajuda">Auto-ajuda</a>
+            <Link to='/mapa-mental' className="MenuItem">Mapa mental</Link>
+            <Link to='/pacientes' className="MenuItem">pacientes</Link>
+            <Link to='/autoajuda' className="MenuItem">Auto-ajuda</Link>
+            <Link to='/login' className="MenuItem">login</Link>
+            <Link to='/psicologos' className="MenuItem">psicologos</Link>
             <a className="MenuItem" href="">Pacientes</a>
             <a className="MenuItem" href="">Psicólogos</a>
           

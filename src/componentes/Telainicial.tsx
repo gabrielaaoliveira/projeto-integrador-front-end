5d2a2004589a9b21d1ba34784f84e2b4db443ce5
Gabriela Oliveira <gabriella04022003@gmail.com>
2022-07-05 21:51:08 -0300

    .

--- src/componentes/Telainicial.tsx
+++ src/componentes/Telainicial.tsx
@@ -13,43 +13,10 @@ const Telainicial = function () {
     <img src={banner2} alt="" />
     
 
-    <div className='TelaMapa'>
-                <div className='Mapas'>
-                    <div className='Mapa'>
-                        <div className='Imagem'>
-                            <img src='https://th.bing.com/th/id/OIP.zMff27eoUnwq215CHawl4gHaE9?pid=ImgDet&rs=1' alt='Ansiedade' />
-                        
-                        </div>
+   
             
-                        <div className='Texto'>
-                            <p> 
-                                <h4>ANSIEDADE</h4>
-                            No Brasil 9,3% de pessoas sofrem de ansiedade, equivalente á 18 milhões de brasileiros, você não está sozinho nessa!!
-                            </p>
-                        </div>
-                    </div>
-
-                    <div className='Mapa'>
-                        <div className='Imagem'>
-                            <img src='https://cdn.fatimanews.com.br/upload/dn_arquivo/2020/07/noti-cia-1-esquizofrenia.png' alt='Esquizofrenia' />
-                        </div>
-                       
-                        <div className='Texto'>
                     
-                        </div>
-                        <div className='Texto'>
-                            <p>
-                                <h4></h4>
-                               
-                            </p>
-                        </div>
-                    </div>
-
                 
-                        </div>
-                    </div>
-            
-            
     
         </>
     );
